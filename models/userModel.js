9c6acb6c798adedd839397bd288dc06bd30e8822
13urn0ut <nitrojunky@gmail.com>
2025-02-17 09:12:05 +0200

    Fix user model

--- models/userModel.js
+++ models/userModel.js
@@ -11,45 +11,21 @@ exports.createUser = async (user) => {
 };
 
 exports.getUserById = async (id) => {
-  const user = await sql.begin(async () => {
-    const [user] = await sql`
+  const [user] = await sql`
     SELECT *
     FROM users
     WHERE id = ${id}
   `;
 
-    if (!user) return;
-
-    user.pets = await sql`
-    SELECT pets.id, pets.name
-    FROM pets
-    WHERE pets.user_id = ${user?.id}
-  `;
-
-    return user;
-  });
-
   return user;
 };
 
 exports.getUserByEmail = async (email) => {
-  const user = await sql.begin(async () => {
-    const [user] = await sql`
+  const [user] = await sql`
     SELECT *
     FROM users
     WHERE email = ${email}
   `;
-
-    if (!user) return;
-
-    user.pets = await sql`
-    SELECT pets.id, pets.name
-    FROM pets
-    WHERE pets.user_id = ${user?.id}
-  `;
-
-    return user;
-  });
 
   return user;
 };
